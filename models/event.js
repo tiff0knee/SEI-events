ea8ef7f809f0a19899e6e5255fdd92e744477e4f
Tiffany <tiffany.lam@gmail.com>
2022-01-11 21:56:38 -0500

    update new and show views

--- models/event.js
+++ models/event.js
@@ -4,16 +4,25 @@ const Schema = mongoose.Schema;
 
 //should have an event array with a list of events that each student has joined that is inside the student model
 
+
+const attendeesSchema = new Schema({
+    attendeeName: String
+
+})
+
 const eventSchema = new Schema({
+    creator: String,
     eventName: String,
     cohort: Number,
     date: Date,
-    attendees: [String],
-    topics: [String]},
+    topics: [String],
+    attendees: [attendeesSchema]},
+
     {
         timestamp: true,
     }
     
 )
+
 
 module.exports = mongoose.model('Event', eventSchema);
